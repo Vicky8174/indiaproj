b2201bcc0e8ddcb615eb2688fa7c1c20c875952f
Vicky Jha <vjha0097@gmail.com>
2026-01-16 16:16:00 +0530

    Add JUnit test class for MainClass

--- src/main/java/com/wip/MainClass.java
+++ src/main/java/com/wip/MainClass.java
@@ -1,13 +1,21 @@
 package com.wip;
 
-/**
- * Hello world!
- *
- */
 public class MainClass
 {
+	
+	public int add(int... number) {
+		int result=0;
+		for(int i : number) {
+			result = result +i;
+		}		
+		return result;
+	}
+	
+	public int multiply(int a, int b) {
+		return a*b;
+	}
     public static void main( String[] args )
     {
-        System.out.println( "Java within Docker -- Maven Project!!!" );
+        System.out.println( "Java within Jenkins -- Maven Project!!!" );
     }
 }
